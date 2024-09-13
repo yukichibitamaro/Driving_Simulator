a8e4b02b233e22e7f6446473097ddaad15c8f2b0
YuyaShinagawa_LAPTOP <yuya4730@gmail.com>
2024-09-14 01:29:14 +0900

    webhook連携テスト

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 class Car {
 public:
     Car() {
-        shape.setSize(sf::Vector2f(50, 500));
+        shape.setSize(sf::Vector2f(50, 1000));
         shape.setFillColor(sf::Color::Black);
         shape.setPosition(375, 270);
         velocity = 0.1f;
